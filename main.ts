0bfd57bdbd784b9cc3c4846d9842db27da36ef07
Daniel Kristaly <kristaly.daniel92@gmail.com>
2019-10-03 16:47:42 +0200

    struggling with remove

--- main.ts
+++ main.ts
@@ -1,11 +1,13 @@
 'use strict';
 
+import { parse } from "path";
+
 const fs = require ('fs');
 const args: string[] = process.argv;
 const usageInfo: string = fs.readFileSync('usageInfo.txt', 'utf-8');
 
 let toDo: string = fs.readFileSync('todo_list.txt', 'utf-8');
-const toDoLines: string[] = toDo.split('\n'); //split the text message by new lines
+let toDoLines: string[] = toDo.split('\n'); //split the text message by new lines
 
 let mainProcess = () => {
   if (args.length == 2) {
@@ -25,6 +27,16 @@ let mainProcess = () => {
       fs.appendFileSync('todo_list.txt', '\n' + args[3] ,'utf-8');
       toDo = fs.readFileSync('todo_list.txt', 'utf-8');
       console.log(toDo);
+    }
+  } else if (args[2] == '-r') {
+    if (args[3] == undefined){ //no index option
+      console.log('Unable to remove: no index provided');
+    } else if (10-5 == 0){ // too high number option
+
+    } else if (parseInt(args[3]) == NaN) {  //not a number option
+      console.log('Unable to remove: index is not a number')
+    } else { 
+      toDoLines.splice(parseInt(args[3]) - 1, 1);
     }
   }
 } 
